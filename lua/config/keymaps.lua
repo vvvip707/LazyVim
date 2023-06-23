a469f96035b92b9700e2b7dd3626ddd0ca16e8a9
ydefu <ydefuA@aliyun.com>
2023-06-23 21:45:00 +0800

    add keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,20 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
+
+local keymaps = vim.keymap
+keymaps.set("i", "jk", "<esc>")
+keymaps.set("n", "<leader>ww", "<esc>:w<cr>")
+keymaps.set("n", "<leader>xx", "<esc>:x<cr>")
+keymaps.set("n", "<leader>qq", "<esc>:q!<cr>")
+keymaps.set("n", "<leader>wa", "<esc>:wa<cr>")
+keymaps.set("n", "<leader>xa", "<esc>:xa<cr>")
+keymaps.set("n", "<leader>qa", "<esc>:qa!<cr>")
+keymaps.set("n", "<leader>tn", "<esc>:tab new<cr>")
+keymaps.set("n", "<C-J>", "<C-W><C-J>")
+keymaps.set("n", "<C-K>", "<C-W><C-K>")
+keymaps.set("n", "<C-H>", "<C-W><C-H>")
+keymaps.set("n", "<C-L>", "<C-W><C-L>")
