ed024a3ba2bb0655764a45a183669c352c5bbbed
ydefu <ydefuA@aliyun.com>
2023-07-01 19:09:08 +0800

    ccls缓存目录放/tmp/ccls

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -7,7 +7,19 @@ return {
       servers = {
         -- pyright will be automatically installed with mason and loaded with lspconfig
         pyright = {},
-        ccls = {},
+        ccls = {
+          init_options = {
+            index = {
+              threads = 0,
+            },
+            cache = {
+              directory = '/tmp/ccls',
+            },
+            hilight = {
+              IsRange = true,
+            }
+          }
+        },
         bashls = {},
         jsonls = {},
         awk_ls = {},
