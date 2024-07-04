df6d9cc985f29c4df659aa6ff3487c83e5762a22
ydefu <ydefuA@aliyun.com>
2024-07-04 22:30:41 +0800

    telescope path display optimization

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,3 +1,14 @@
 return {
-    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    { 'nvim-telescope/telescope.nvim',
+        config = function ()
+            local telescope = require('telescope')
+            telescope.setup({
+                defaults = {
+                    dynamic_preview_title = true,
+                    path_display = { 'shorten' },
+                }
+            })
+        end
+    },
 }
