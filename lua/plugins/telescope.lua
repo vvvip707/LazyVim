99393320ace0f901d88dbeb470eddf2ca0977b65
ydefu <ydefuA@aliyun.com>
2024-07-06 00:11:27 +0800

    telescope, config the layout

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,14 +1,21 @@
 return {
-    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    { 'nvim-telescope/telescope.nvim',
-        config = function ()
-            local telescope = require('telescope')
-            telescope.setup({
-                defaults = {
-                    dynamic_preview_title = true,
-                    path_display = { 'shorten' },
-                }
-            })
-        end
-    },
+  { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+  {
+    "nvim-telescope/telescope.nvim",
+    config = function()
+      local telescope = require("telescope")
+      telescope.setup({
+        defaults = {
+          dynamic_preview_title = true,
+          path_display = { "shorten" },
+          layout_config = {
+            horizontal = {
+              width = 0.9,
+              preview_width = 0.55,
+            },
+          },
+        },
+      })
+    end,
+  },
 }
